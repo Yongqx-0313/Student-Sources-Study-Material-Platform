0e4ba93108e497bee75b3a399c017a676f937891
Yongqx-0313 <morrisqxqx@gmail.com>
2025-08-16 10:45:06 +0800

    add upload btn

--- Main.php
+++ Main.php
@@ -27,6 +27,7 @@
         <option value="cheatsheet">Cheat Sheet</option>
       </select>
       <button class="bg-indigo-600 text-white px-4 py-2 rounded">Filter</button>
+      <button class="bg-indigo-600 text-white px-4 py-2 rounded">Upload</button>
     </form>
   </div>
 
--- Profile.php
+++ Profile.php
@@ -78,6 +78,7 @@ class="min-h-screen">
                             <button class="bg-blue-600 text-white px-6 py-2 rounded-lg shadow-md hover:bg-blue-700 transition">
                                 Edit Profile
                             </button>
+                            
                         </a>
                     </div>
                 </div>
